9dd9cc88ac317919576438f11dc67229b14e2391
Akhil Pillai <gh@akpi.is-a.dev>
2024-11-17 19:41:08 -0800

    fmt & add links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Link from '@/components/misc/link';
+
 export default function Home() {
 	return (
-		<main className="min-h-screen grid place-content-center bg-blue dark:bg-purple">
+		<main className="min-h-screen grid place-content-center bg-blue dark:bg-purple gap-8">
 			<h1 className="text-2xl font-bold font-serif">
 				The ✨Official✨ Kumilinwa Dictionary
 			</h1>
+			<Link
+				href="/search/"
+				className="text-xl font-bold text-purple dark:text-yellow"
+			>
+				Search
+			</Link>
+			<Link
+				href="/browse/"
+				className="text-xl font-bold text-purple dark:text-yellow"
+			>
+				Browse
+			</Link>
 		</main>
 	);
 }
